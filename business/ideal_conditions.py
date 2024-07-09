eca40ab1fb091982284bd706fca3710e0ccc63ec
Charles Grace <cbgrace479@gmail.com>
2024-07-09 16:32:54 -0500

    working on sorting the forecast data...

--- business/ideal_conditions.py
+++ business/ideal_conditions.py
@@ -9,3 +9,23 @@ The criteria are somewhat a work in progress, but here is what I am thinking:
 - Good conditions = overcast
 - Obviously no rain
 """
+from models import Forecast
+
+
+def sort_forecast_list(forecast_list: [Forecast]) -> []:
+    """
+    Hopefully this method will sort a list of Forecast data and return dates & times that match my ideal criteria
+    :param forecast_list:
+    :return:
+    """
+    # so I need to take the list, and look for 2 hours of 'not rain' before (at least) an hour of rain
+    # result_dict = {}
+    # rain_keys = [k for k, v in weather_dict.items() if v == 'rain']
+    #
+    # for key in rain_keys:
+    #     for offset in range(-2, 3):  # range from -2 to 2
+    #         check_key = key + offset
+    #         if check_key in weather_dict and check_key not in result_dict:
+    #             result_dict[check_key] = weather_dict[check_key]
+    #
+    # print(result_dict)
